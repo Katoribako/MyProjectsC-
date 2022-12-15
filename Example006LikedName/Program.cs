c4449358f1d3f54540db94c50b11d458e3dd2188
Katoribako <64684310+Katoribako@users.noreply.github.com>
2022-12-15 15:04:37 +0300

    Added LikedName Programm

---
+++ Example006LikedName/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Please enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "bob")
+{
+    Console.WriteLine("Welcome home Bob!");
+}
+else
+{
+    Console.Write("Hello ");
+    Console.WriteLine(username);
+}
