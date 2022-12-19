077c76646ba5a9e4c8b5ccd09ad87938da144494
Katoribako <64684310+Katoribako@users.noreply.github.com>
2022-12-19 17:30:39 +0300

    Added AnotherArray

---
+++ Example011AnotherArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {15, 27, 610, 46, 52, 610, 78, 82};
+
+int n = array.Length;
+int find = 610;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //Made so that it finds only one particle of the array
+    }
+    index++;
+}
