12461178cc735ffd8d10a89fe6e0f23968f31216
Katoribako <64684310+Katoribako@users.noreply.github.com>
2023-01-11 17:35:44 +0300

    Added 4 type method example

---
+++ Example017Method4Type/Program.cs
@@ -0,0 +1,13 @@
+﻿string method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + c;
+        i ++;
+    }
+    return result;
+}
+string res = method4(c: "jarooom", count: 8);
+Console.WriteLine(res);
