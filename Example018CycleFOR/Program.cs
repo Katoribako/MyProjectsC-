c9cc03ad6ed1032fb1084943caa030a21aaa743f
Katoribako <64684310+Katoribako@users.noreply.github.com>
2023-01-11 18:14:00 +0300

    added cycle for example018

---
+++ Example018CycleFOR/Program.cs
@@ -0,0 +1,11 @@
+﻿string method4(string msg, int count)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i ++)
+    {
+        result = result + msg;
+    }
+    return result;
+}
+string res = method4(msg: "test", count: 5);
+Console.WriteLine(res);
