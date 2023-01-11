f4ebc5d0592940b3f5c2471c6435fcb5515ae759
Katoribako <64684310+Katoribako@users.noreply.github.com>
2023-01-11 16:54:08 +0300

    added ex15 with type 2 method

---
+++ Example015Type2/Program.cs
@@ -0,0 +1,11 @@
+﻿void method2(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i ++;
+    }
+}
+method2(msg:"text", count: 5);
+method2(count: 5, msg:"there you are"); //это одно и то же
